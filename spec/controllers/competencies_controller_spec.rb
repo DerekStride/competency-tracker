4c4168ffa2a829de259fa3e6fcb74d531b9aa009
Babak Esfandiari <babak.esfandiari@shopify.com>
2016-04-06 17:45:21 -0400

    cleaned up competency controller spec

--- spec/controllers/competencies_controller_spec.rb
+++ spec/controllers/competencies_controller_spec.rb
@@ -11,8 +11,7 @@ RSpec.describe CompetenciesController, type: :controller do
     it "returns all the competencies" do
       FactoryGirl.create :competency
       FactoryGirl.create :competency, name: "Can juggle"
-      #get :index
-      get :index, {}, { "Accept" => "application/json" }
+      get :index
       body = JSON.parse(response.body)
       competency_names = body.map { |c| c["name"]}
 
@@ -20,21 +19,5 @@ RSpec.describe CompetenciesController, type: :controller do
     end
 
   end
-
-  # describe "GET /movies" do
-  #   it "returns all the movies" do
-  #     FactoryGirl.create :movie, name: "The Lord of the Rings"
-  #     FactoryGirl.create :movie, name: "The Two Towers"
-
-  #     get "/movies", {}, { "Accept" => "application/json" }
-
-  #     expect(response.status).to eq 200
-
-  #     body = JSON.parse(response.body)
-  #     movie_titles = body.map { |m| m["title"] }
-
-  #     expect(movie_titles).to match_array(["The Lord of the Rings",
-  #                                          "The Two Towers"])
-  #   end
 
 end
