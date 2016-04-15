420a5d3e859b5a7f54aeb855e602b5c4fb678c6e
Babak Esfandiari <babak.esfandiari@shopify.com>
2016-04-15 14:37:23 -0400

    attempt to test PUT, which should fail due to use of learnable

--- spec/controllers/competencies_controller_spec.rb
+++ spec/controllers/competencies_controller_spec.rb
@@ -20,4 +20,21 @@ RSpec.describe CompetenciesController, type: :controller do
 
   end
 
+  describe "PUT /competencies" do
+  	it "creates a competency successfully" do
+  	  competency_params = {
+  	  	"competency" => {
+  	  		"name" => "Can solve Rubik's cube"
+  	  	}
+  	  }.to_json
+  	  post "/competencies.json", competency_params
+  	  expect(response).to have_http_status(201)
+  	  get :index
+  	  body = JSON.parse(response.body)
+      competency_names = body.map { |c| c["name"]}
+
+      expect(competency_names).to match_array(["Can solve Rubik's cube"])
+    end
+  end
+
 end
