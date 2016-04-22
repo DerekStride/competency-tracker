f777482409400019cb92380d808ae3934b4db3c4
Babak Esfandiari <babak.esfandiari@shopify.com>
2016-04-22 15:06:41 -0400

    DRY'd up controller tests

--- spec/controllers/competencies_controller_spec.rb
+++ spec/controllers/competencies_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe CompetenciesController, type: :controller do
+
+  before :each do
+  	FactoryGirl.create :competency, name: "Can solve Rubik's cube"
+  end
+
   describe "GET #index" do
     it "responds successfully with an HTTP 200 status code" do
       get :index
@@ -9,34 +14,25 @@ RSpec.describe CompetenciesController, type: :controller do
     end
 
     it "returns all the competencies" do
-      FactoryGirl.create :competency
       FactoryGirl.create :competency, name: "Can juggle"
       get :index
       body = JSON.parse(response.body)
       competency_names = body.map { |c| c["name"]}
 
-      expect(competency_names).to match_array(["Nginx", "Can juggle"])
+      expect(competency_names).to match_array(["Can solve Rubik's cube", "Can juggle"])
     end
 
   end
 
   describe "POST /competencies" do
-  	it "creates a competency successfully" do
-  	  post :create, competency: { name: "Can solve Rubik's cube" }
-
-      expect(Competency.first.name).to eq "Can solve Rubik's cube"
-
+  	it "creates a competency successfully" do 	 
+      expect {post :create, competency: { name: "Can juggle" }}.to change{Competency.count}.by(1)
     end
   end
 
   describe "PUT /competencies/1" do
   	it "updates a competency successfully" do
-  	  post :create, competency: { name: "Can solve Rubik's cube" }
-  	  
-  	  get :index
-  	  body = JSON.parse(response.body)
-  	  id = body.first["id"]
-
+  	  id = Competency.first.id
   	  put :update, id: id, competency: { name: "Can solve Sudoku" }
  
       expect(Competency.first.name).to eq "Can solve Sudoku"
@@ -44,16 +40,10 @@ RSpec.describe CompetenciesController, type: :controller do
     end
 
     it "successfully updates a competency by adding a sub-competency" do
-      post :create, competency: { name: "Can solve Rubik's cube" }
       post :create, competency: { name: "Can solve it in 30 seconds or less"}
 
-  	  get :index
-  	  body = JSON.parse(response.body)
-  	  competency_names = body.map { |c| c["name"]}
-  	  expect(competency_names).to match_array(["Can solve Rubik's cube", "Can solve it in 30 seconds or less"])
-
-  	  id = body.first["id"]
-  	  id2 = body.last["id"]
+  	  id = Competency.first.id
+  	  id2 = Competency.last.id
 
   	  # make id2 the sub-competency of id
   	  put :update, id: id, competency: {competency: {id: id2} }
